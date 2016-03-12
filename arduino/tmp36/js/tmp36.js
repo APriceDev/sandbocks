ba682849deb62c10bb42a8379cfc3edea03b4db9
APriceDev <alanprice.dev1@gmail.com>
2016-03-11 20:25:45 -0500

    added tmp36.js

---
+++ arduino/tmp36/js/tmp36.js
@@ -0,0 +1,31 @@
+var five = require("johnny-five");
+var board = new five.Board();
+var moment = require("moment");
+var title = moment().format("YYYYMMDD-HHmmss");
+var fs = require("fs");
+var outputFile = fs.openSync("data/" + title + ".txt", "w");
+
+board.on("ready", function() {
+  console.log("Board ready");
+
+  var tmp36 = new five.Thermometer({
+    controller: "TMP36",
+    pin: "A0",
+    freq: 5000
+  });
+
+  board.repl.inject({
+    tmp36 : tmp36
+  })
+
+  tmp36.on("data", function() {
+
+    var f = this.F.toFixed(2);
+    var timeStamp = moment().format("MMDD-HHmmss");
+    var output = timeStamp + " " + f + " °F";
+    fs.writeSync(outputFile, output + "\r\n");
+    console.log(output);
+  });
+
+});
+console.log("Waiting for device to connect...");
