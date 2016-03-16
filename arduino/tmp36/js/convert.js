5ae65efcc68c39628d4acb2938a6d6c3fed19d40
APriceDev <alanprice.dev1@gmail.com>
2016-03-16 15:02:48 -0400

    small update convert.js

--- arduino/tmp36/js/convert.js
+++ arduino/tmp36/js/convert.js
@@ -1,14 +1,12 @@
-//
 
 var fs = require("fs");
 
 var file = "../data/" + process.argv[2];
 
-
 fs.readFile(file, "utf8", function(err, data){
     //console.log(data);
 
-    var arr = data.toString().split("\n");
+    var arr = data.split(/\r?\n/);
 
     for(i in arr){
         console.log(JSON.stringify(arr[i]));
