0b16b6378bcc83b32bdc28ebec8fd4abc815c661
APriceDev <alanprice.dev1@gmail.com>
2016-04-20 20:46:49 -0400

    refactoring tmp36.js

--- arduino/tmp36/js/tmp36.js
+++ arduino/tmp36/js/tmp36.js
@@ -1,34 +1,31 @@
-var five = require("johnny-five");
+var five = require('johnny-five');
 var board = new five.Board();
-var moment = require("moment");
-var title = moment().format("YYYYMMDD-HHmmss");
-var fs = require("fs");
-//var outputFile = fs.openSync("data/" + title + ".txt", "w");
-var outputFile = fs.openSync("../data/" + title + ".txt", "w");
+var moment = require('moment');
+var title = moment().format('YYYYMMDD-HHmmss');
+var fs = require('fs');
+var outputFile = fs.openSync('data/'  + title + '.txt', 'w');
 
-board.on("ready", function() {
-  console.log("Board ready");
+board.on('ready', function(){
+    console.log('board ready');
 
-  var tmp36 = new five.Thermometer({
-    controller: "TMP36",
-    pin: "A0",
-    freq: 5000
-  });
+    var tmp36 = new five.Thermometer({
+        controller : 'TMP36',
+        pin : 'A0',
+        freq : 5000
+    });
 
-  board.repl.inject({
-    tmp36 : tmp36
-  })
+    // may not need the repl but good to have around
+    board.repl.inject({
+        tmp36 : tmp36
+    });
 
-  tmp36.on("data", function() {
-
-    var f = this.F.toFixed(2);
-    var timeStamp = moment().format("YYYYMMDDHHmmss");
-    //var output = timeStamp + " " + f + " °F";
-    //var output = "{time : " + timeStamp + ", temp : " + f + "}";
-    var output = "{\"time\" : " + timeStamp + ", \"temp\" : " + f + "}";
-    fs.writeSync(outputFile, output + "\r\n");
-    console.log(output);
-  });
-
+    tmp36.on('data', function(){
+        var f = this.F.toFixed(2);
+        var timestamp = moment().format('HH:mm:ss');
+        //var output = '{\'time\' : ' + timestamp + ', \'temp\' : ' + f + '}';
+        var output = '{\'time\' : ' +  '\'' + timestamp + '\'' + ', \'temp\' : ' + f + '}';
+        fs.writeSync(outputFile, output + '\r\n');
+        console.log(output);
+    });
 });
-console.log("Waiting for device to connect...");
+console.log('waiting to connect...');
