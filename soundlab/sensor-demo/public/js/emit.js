3c0917835906c17851fff1cf0e551dd086a24e6d
APriceDev <alanprice.dev1@gmail.com>
2016-05-12 21:03:42 -0400

    update css

--- soundlab/sensor-demo/public/js/emit.js
+++ soundlab/sensor-demo/public/js/emit.js
@@ -4,8 +4,6 @@ var sensor02 = document.querySelector('.sensor02');
 
 socket.on('sensor01', function(message){
     sensor01.innerHTML = parseInt(message);
-    //var hue = 200 - (parseInt(message) * 5);
-    //document.body.style.backgroundColor = 'hsl(' + hue + ', 30%, 25%)';
 });
 
 socket.on('sensor02', function(message){
