a91f6a6371966c964d50fa1dcb4e14ebb2c6938d
APriceDev <alanprice.dev1@gmail.com>
2016-10-11 22:06:55 -0400

    added collision detection to breakout.js

--- gamedev/public/js/breakout.js
+++ gamedev/public/js/breakout.js
@@ -37,11 +37,12 @@
     let brickOffsetTop = 30;
     let brickOffsetLeft = 30;
     let bricks = [];
+    let score = 0;
 
     for(let c = 0; c < brickColumnCount; c++){
         bricks[c] = [];
         for(let r = 0; r < brickRowCount; r++){
-            bricks[c][r] = {x : 0, y : 0; status : 1};
+            bricks[c][r] = {x : 0, y : 0, status : 1};
         };
     };
 
@@ -64,17 +65,44 @@
     function drawBricks(){
         for(let c = 0; c < brickColumnCount; c++){
             for(let r = 0; r < brickRowCount; r++){
-                let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
-                let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
-                bricks[c][r].x = brickX;
-                bricks[c][r].y = brickY;
-                context.beginPath();
-                context.rect(brickX, brickY, brickWidth, brickHeight);
-                context.fillStyle = '#0095DD';
-                context.fill();
-                context.closePath();
+                if(bricks[c][r].status == 1){
+                    let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+                    let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+                    bricks[c][r].x = brickX;
+                    bricks[c][r].y = brickY;
+                    context.beginPath();
+                    context.rect(brickX, brickY, brickWidth, brickHeight);
+                    context.fillStyle = '#0095DD';
+                    context.fill();
+                    context.closePath();
+                }
             }
         }
+    };
+
+    function collisionDetection(){
+        for(let c = 0; c < brickColumnCount;c++){
+            for(let r = 0; r < brickRowCount; r++){
+                let b = bricks[c][r];
+                if(b.status == 1){
+                    if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight){
+                        dy = -dy;
+                        b.status = 0;
+                        score++;
+                        if(score === brickRowCount * brickColumnCount){
+                            alert('yay, team...');
+                            document.location.reload();
+                        }
+                    }
+                }
+            }
+        };
+    };
+
+    function drawScore(){
+        context.font = '16px Arial';
+        context.fillStyle = '#0095DD';
+        context.fillText('Score: ' + score, 8, 20);
     };
 
     function draw(){
@@ -82,6 +110,8 @@
         drawBricks();
         drawBall();
         drawPaddle();
+        collisionDetection();
+        drawScore();
 
         if(x + dx > canvas.width - ballRadius || x + dx < ballRadius){dx = -dx};
         if(y + dy < ballRadius){
@@ -90,8 +120,8 @@
             if(x > paddleX && x < paddleX + paddleWidth){
                 dy = -dy;
             }else{
-                console.log('game over, man!');
-                // document.location.reload();
+                alert('game over, man!');
+                document.location.reload();
             }
         };
 
