b716f7265b28304d86ca225435153da14a434f39
APriceDev <alanprice.dev1@gmail.com>
2017-04-24 22:07:43 -0400

    refactor gravitybomb main.js

--- poc/gameplay/gravitybomb/main.js
+++ poc/gameplay/gravitybomb/main.js
@@ -1,32 +1,33 @@
 const canvas = document.querySelector('#range');
-//const calibrate = document.querySelector('input[type="range"');
-const calibrate = document.querySelector('#calibrate');
-const cycle= document.querySelector('#cycle');
 const ctx = canvas.getContext('2d');
+//const calibrate = document.querySelector('input[type="range"]');
+const calibrate = document.querySelector('#calibrate');
+const cycle = document.querySelector('#cycle');
 
-let posX = 0;
-let posY = 200;
-let rate = 1;
+// convert dashboard props to blip constructor
+const dashboard = {
+    posX : 0,
+    posY : 200,
+    rate : 1
+}
+let {posX, posY, rate} = dashboard;
 
-
-    ctx.fillStyle ='rgba(0, 50, 0, 1)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle ='rgba(0, 50, 0, 1)';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 window.requestAnimationFrame(draw);
 
 function draw(){
-    //console.log(posX);
-
     ctx.fillStyle ='rgba(0, 50, 0, 0.5)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    // convert to blip constructor
     ctx.fillStyle = 'rgba(0, 175, 0, 0.65)';
     ctx.fillRect(posX, posY , 10, 10);
-
     posX += rate;
     if(posX >= canvas.width){ posX = 0}
 
-    window.requestAnimationFrame(draw);
+        window.requestAnimationFrame(draw);
 }
 
 function yChange(){
@@ -36,12 +37,12 @@ function yChange(){
 
 function xChange(){
     rate = parseInt(this.value);
-    console.log(this.value);
+    //console.log(this.value);
 }
 
-function logEvent(e){
-    console.log(this.value);
-}
+// function logEvent(e){
+//     console.log(e);
+// }
 
 calibrate.addEventListener('input', yChange);
 cycle.addEventListener('input', xChange);
