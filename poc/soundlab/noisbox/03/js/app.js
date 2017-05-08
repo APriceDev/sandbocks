bd1658907d7feb45de2e257eea7a0396945c7e22
APriceDev <alanprice.dev1@gmail.com>
2017-05-08 18:33:43 -0400

    update range sliders css + js

--- poc/soundlab/noisbox/03/js/app.js
+++ poc/soundlab/noisbox/03/js/app.js
@@ -1,15 +1,33 @@
-const vol = document.querySelector('#volume');
-const volRange = vol.querySelector('#volumeRange');
+const vol = document.querySelector('#vol');
+const volBar = vol.querySelector('#vol-bar');
+const freq = document.querySelector('#freq');
+const freqBar = vol.querySelector('#freq-bar');
 
-  function handleMove(e) {
-      const x = e.pageX - this.offsetLeft;
-      const percent = x / this.offsetWidth;
-      const min = 0;
-      const max = 100;
-      const width = Math.round(percent * 100) + '%';
-      const volume = percent * (max - min) + min;
-      volRange.style.width = width;
-      volRange.textContent = Math.round(volume.toFixed(2)) + '%';
-      //osc.volume = volume;
-    }
-  vol.addEventListener('mousemove', handleMove);
+let oscVolume = 50;
+let ossFrequency = 440;
+
+function updateVol(value){
+    oscVolume = value;
+}
+
+function updateFreq(value){
+    oscFrequency = value;
+}
+
+
+function handleMove(e, el, minValue, maxValue, callback, place, str) {
+    const x = e.pageX - el.offsetLeft;
+    const percent = x / el.offsetWidth;
+    const min = minValue;
+    const max = maxValue;
+    const width = Math.round(percent * 100) + '%';
+    const value = percent * (max - min) + min;
+    el.firstChild.style.width = width;
+    el.firstChild.textContent = Math.round(value.toFixed(place)) + str;
+    callback(value)
+}
+
+vol.addEventListener('mousemove', e => handleMove(e, vol, 0, 100, updateVol, 2, '%'));
+freq.addEventListener('mousemove', e => handleMove(e, freq, 0, 5000, updateFreq, 4, 'hz'));
+  //vol.addEventListener('mousemove', handleMove);
+  //freq.addEventListener('mousemove', handleMove);
