ffb051aa4917c940a7e2d2e950e2e1dd842d74b5
APriceDev <alanprice.dev1@gmail.com>
2017-05-07 22:30:13 -0400

    add slider fn

--- poc/soundlab/noisbox/03/js/app.js
+++ poc/soundlab/noisbox/03/js/app.js
@@ -1,1 +1,15 @@
-console.log('pronto!')
+const vol = document.querySelector('#volume');
+const volRange = vol.querySelector('#volumeRange');
+
+  function handleMove(e) {
+      const x = e.pageX - this.offsetLeft;
+      const percent = x / this.offsetWidth;
+      const min = 0;
+      const max = 100;
+      const width = Math.round(percent * 100) + '%';
+      const volume = percent * (max - min) + min;
+      volRange.style.width = width;
+      volRange.textContent = Math.round(volume.toFixed(2)) + '%';
+      //osc.volume = volume;
+    }
+  vol.addEventListener('mousemove', handleMove);
