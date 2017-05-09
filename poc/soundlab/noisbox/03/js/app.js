370b26b72c6cbfb30dfae7e40d1168cbfdbd82f5
APriceDev <alanprice.dev1@gmail.com>
2017-05-08 22:23:01 -0400

    added js waveform button functionality

--- poc/soundlab/noisbox/03/js/app.js
+++ poc/soundlab/noisbox/03/js/app.js
@@ -1,8 +1,12 @@
+const dashboard = document.querySelector('#dashboard');
+const waveforms = document.querySelectorAll('[data-wavform]');
 const vol = document.querySelector('#vol');
 const volBar = vol.querySelector('#vol-bar');
 const freq = document.querySelector('#freq');
 const freqBar = vol.querySelector('#freq-bar');
 
+let power = false;
+let waveform = 'sine';
 let oscVolume = 50;
 let ossFrequency = 440;
 
@@ -14,20 +18,30 @@ function updateFreq(value){
     oscFrequency = value;
 }
 
-
-function handleMove(e, el, minValue, maxValue, callback, place, str) {
+function handleRange(e, el, min, max, callback, place, str) {
     const x = e.pageX - el.offsetLeft;
     const percent = x / el.offsetWidth;
-    const min = minValue;
-    const max = maxValue;
-    const width = Math.round(percent * 100) + '%';
     const value = percent * (max - min) + min;
-    el.firstChild.style.width = width;
+    el.firstChild.style.width = Math.round(percent * 100) + '%';
     el.firstChild.textContent = Math.round(value.toFixed(place)) + str;
     callback(value)
 }
 
-vol.addEventListener('mousemove', e => handleMove(e, vol, 0, 100, updateVol, 2, '%'));
-freq.addEventListener('mousemove', e => handleMove(e, freq, 0, 5000, updateFreq, 4, 'hz'));
-  //vol.addEventListener('mousemove', handleMove);
-  //freq.addEventListener('mousemove', handleMove);
+function handleWaveForm(e){
+    if (e.target.id != 'dashboard'){
+        if (e.target.id === 'power'){
+            e.target.classList.toggle('active');
+            power = !power;
+        }else{
+            waveforms.forEach((wave) => {
+                wave.classList.remove('active');
+            });
+            e.target.classList.add('active');
+            waveform = e.target.id;
+        }
+    }
+}
+
+dashboard.addEventListener('click', handleWaveForm);
+vol.addEventListener('mousemove', e => handleRange(e, vol, 0, 100, updateVol, 2, '%'));
+freq.addEventListener('mousemove', e => handleRange(e, freq, 0, 5000, updateFreq, 4, 'hz'));
