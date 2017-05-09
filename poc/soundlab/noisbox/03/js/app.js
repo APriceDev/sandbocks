8d648eb3c349571ad8f426e6dbc1ecb963aee15e
APriceDev <alanprice.dev1@gmail.com>
2017-05-09 17:57:49 -0400

    added iife wrapper to app.js

--- poc/soundlab/noisbox/03/js/app.js
+++ poc/soundlab/noisbox/03/js/app.js
@@ -1,47 +1,54 @@
-const dashboard = document.querySelector('#dashboard');
-const waveforms = document.querySelectorAll('[data-wavform]');
-const vol = document.querySelector('#vol');
-const volBar = vol.querySelector('#vol-bar');
-const freq = document.querySelector('#freq');
-const freqBar = vol.querySelector('#freq-bar');
+(function(){
 
-let power = false;
-let waveform = 'sine';
-let oscVolume = 50;
-let ossFrequency = 440;
+    const dashboard = document.querySelector('#dashboard');
+    const waveforms = document.querySelectorAll('[data-wavform]');
+    const vol = document.querySelector('#vol');
+    const volBar = vol.querySelector('#vol-bar');
+    const freq = document.querySelector('#freq');
+    const freqBar = vol.querySelector('#freq-bar');
 
-function updateVol(value){
-    oscVolume = value;
-}
+    let power = false;
+    let waveform = 'sine';
+    let oscVolume = 50;
+    let ossFrequency = 440;
 
-function updateFreq(value){
-    oscFrequency = value;
-}
+    function updateVol(value){
+        oscVolume = value;
+    }
 
-function handleRange(e, el, min, max, callback, place, str) {
-    const x = e.pageX - el.offsetLeft;
-    const percent = x / el.offsetWidth;
-    const value = percent * (max - min) + min;
-    el.firstChild.style.width = Math.round(percent * 100) + '%';
-    el.firstChild.textContent = Math.round(value.toFixed(place)) + str;
-    callback(value)
-}
+    function updateFreq(value){
+        oscFrequency = value;
+    }
 
-function handleWaveForm(e){
-    if (e.target.id != 'dashboard'){
-        if (e.target.id === 'power'){
-            e.target.classList.toggle('active');
-            power = !power;
-        }else{
-            waveforms.forEach((wave) => {
-                wave.classList.remove('active');
-            });
-            e.target.classList.add('active');
-            waveform = e.target.id;
+    function handleRange(e, el, min, max, callback, place, str) {
+        const x = e.pageX - el.offsetLeft;
+        const percent = x / el.offsetWidth;
+        const value = percent * (max - min) + min;
+        el.firstChild.style.width = Math.round(percent * 100) + '%';
+        el.firstChild.textContent = Math.round(value.toFixed(place)) + str;
+        callback(value)
+    }
+
+    function handleWaveForm(e){
+        if (e.target.id != 'dashboard'){
+            if (e.target.id === 'power'){
+                e.target.classList.toggle('active');
+                power = !power;
+            }else{
+                waveforms.forEach((wave) => {
+                    wave.classList.remove('active');
+                });
+                e.target.classList.add('active');
+                waveform = e.target.id;
+            }
         }
     }
-}
 
-dashboard.addEventListener('click', handleWaveForm);
-vol.addEventListener('mousemove', e => handleRange(e, vol, 0, 100, updateVol, 2, '%'));
-freq.addEventListener('mousemove', e => handleRange(e, freq, 0, 5000, updateFreq, 4, 'hz'));
+    function initFn(){
+        dashboard.addEventListener('click', handleWaveForm);
+        vol.addEventListener('mousemove', e => handleRange(e, vol, 0, 100, updateVol, 2, '%'));
+        freq.addEventListener('mousemove', e => handleRange(e, freq, 0, 5000, updateFreq, 4, 'hz'));
+    }
+
+    return initFn();
+})();
