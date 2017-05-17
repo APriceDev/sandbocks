03ad3fdd831aeadc6d392d4bfd42f904a15431c4
APriceDev <alanprice.dev1@gmail.com>
2017-05-16 20:12:01 -0400

    refactor main.js into modular pattern

--- poc/hadron/js/main.js
+++ poc/hadron/js/main.js
@@ -1,33 +1,30 @@
 
-// (function(){
-//     const canvas = document.querySelector('#hadron');
-//     const ctx = canvas.getContext('2d');
-
-//     let width = canvas.width = window.innerWidth;
-//     let height = canvas.height = window.innerHeight;
-
-//     for(let i = 0; i <=100; i++){
-//         ctx.beginPath();
-//         ctx.moveTo(Math.random() * width, Math.random() * height);
-//         ctx.lineTo(Math.random() * width, Math.random() * height);
-//         ctx.stroke();
-//     }
-// }());
-
-(function(){
+// init module pattern
+const createCanvas = function(width = 800, height = 600){
     const canvas = document.querySelector('#hadron');
     const ctx = canvas.getContext('2d');
 
-    let width = canvas.width = 800;
-    let height = canvas.height = 600;
+    canvas.width = width;
+    canvas.height = height;
+
+    return {
+        canvas : canvas,
+        ctx : ctx
+    }
+}();
+
+// subsequent module pattern
+(function(module){
+
+    const canvas = module.canvas;
+    const ctx = module.ctx;
 
     for(let i = 0; i <=100; i++){
         ctx.beginPath();
-        ctx.moveTo(Math.random() * width, Math.random() * height);
-        ctx.lineTo(Math.random() * width, Math.random() * height);
+        ctx.moveTo(Math.random() * canvas.width, Math.random() * canvas.height);
+        ctx.lineTo(Math.random() * canvas.width, Math.random() * canvas.height);
         ctx.stroke();
     }
 
-    console.log(width);
-    console.log(canvas.height);
-}());
+})(createCanvas);
+
