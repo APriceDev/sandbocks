d797f69cf542ccfa62995fec838fcd5bd20aad8d
APriceDev <alanprice.dev1@gmail.com>
2017-05-22 14:47:52 -0400

    add width + height propeties to canvas return object

--- poc/hadron/js/canvas.js
+++ poc/hadron/js/canvas.js
@@ -5,14 +5,16 @@ hadron.createCanvas = (function(width = 800, height = 600){
     const canvas = document.querySelector('#cern');
     const ctx = canvas.getContext('2d');
 
-    // ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
-    // ctx.fillRect(0, 0, canvas.width, canvas.height);
-
     canvas.width = width;
     canvas.height = height;
 
+    // ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
+    // ctx.fillRect(0, 0, canvas.width, canvas.height);
+
     return {
         canvas : canvas,
-        ctx : ctx
-    }
+        ctx : ctx,
+        width : canvas.width,
+        height : canvas.height
+    };
 }());
