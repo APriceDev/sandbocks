3e2d95a7ae9d331100d093270f7149ae3d3c2313
APriceDev <alanprice.dev1@gmail.com>
2017-06-06 21:04:10 -0400

    added arctangent.js

---
+++ poc/hadron/js/arctangent.js
@@ -0,0 +1,39 @@
+hadron.arctangent = (function(module){
+    const {canvas, ctx, width, height} = module;
+
+    let arrowX = width / 2
+        ,arrowY= height / 2
+        ,dx
+        ,dy
+        ,angle = 0;
+
+        render();
+
+        function render(){
+            ctx.clearRect(0, 0, width, height);
+
+            ctx.save();
+            ctx.translate(arrowX, arrowY);
+            ctx.rotate(angle);
+
+            ctx.beginPath();
+            ctx.moveTo(20, 0);
+            ctx.lineTo(-20, 0);
+            ctx.moveTo(20, 0);
+            ctx.lineTo(10, -10);
+            ctx.moveTo(20, 0);
+            ctx.lineTo(10, 10);
+            ctx.stroke();
+
+            ctx.restore();
+
+            requestAnimationFrame(render);
+        }
+
+        document.body.addEventListener('mousemove', function(){
+            dx = event.clientX - arrowX;
+            dy = event.clientY - arrowY;
+            angle = Math.atan2(dy ,dx);
+        });
+
+}(hadron.createCanvas));
