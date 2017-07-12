b05a793600653ee02f7b086be66b92d3f8152ac6
APriceDev <alanprice.dev1@gmail.com>
2017-07-12 15:03:51 -0400

    refactor and update app.js to include modules folder

--- poc/dataparser/app.js
+++ poc/dataparser/app.js
@@ -1,28 +1,30 @@
 const fs = require('fs');
+const utilities = require('./modules/utilities');
+
+//utilities.foo();
 
 let file = 'data/' + process.argv[2];
 
 fs.readFile(file, 'utf8', function(err, data){
 
     // removes trailing comma JSON.parse() does not allow trailing commas
-    const trim = data.replace(/[\s,]+$/g, '');
+   let trim = utilities.removeComma(data);
    //console.log(trim);
 
-   // trimmed items into array
-   const arr = trim.split(",").map(item => item.trim());
+    // trimmed items into array
+    let arr = utilities.itemsToArray(trim);
     //console.log(arr);
 
     // format items,  ready for JSON
-    const format = arr.map(item => `{"temp" : ${item.split(" ")[0]}, "time" : ${item.split(" ")[1]}}`);
+    let format = utilities.format(arr);
     //console.log(format);
 
     // JSON parse
-    const dataArray = [];
-    const result = format.forEach(item => dataArray.push(JSON.parse(item)));
-    //console.log(dataArray);
+    let parsed = utilities.parsed(format);
+    //console.log(parsed);
 
-    // then pass on the data where needed
-    const final = dataArray.forEach(item => {
+    // then display the data as needed
+    const final = parsed.forEach(item => {
         const date = new Date(item.time)
         console.log(item.temp, date);
     });
