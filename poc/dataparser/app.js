bdc2e8a93564f7e800af14cb132ec15d90a97689
APriceDev <alanprice.dev1@gmail.com>
2017-07-13 14:30:49 -0400

    refactor app.js adding asnych functionality

--- poc/dataparser/app.js
+++ poc/dataparser/app.js
@@ -1,28 +1,27 @@
-
 const fs = require('fs');
 const utilities = require('./modules/utilities');
 
-let file = process.argv[2];
+let pathToFile = process.argv[2];
 
-fs.readFile(file, 'utf8', function(err, data){
+let trim, arr, format, parsed;
 
-    if(err) return  console.log(err);
+function getData(callback){
+    fs.readFile(pathToFile, 'utf8', function(err, data){
+        if(err) throw err;
+        trim = utilities.removeComma(data);
+        arr = utilities.itemsToArray(trim);
+        format = utilities.formatData(arr);
+        parsed = utilities.parseData(format);
+        callback(parsed);
+    });
+};
 
-   let trim = utilities.removeComma(data);
-   //console.log(trim);
-
-    let arr = utilities.itemsToArray(trim);
-    //console.log(arr);
-
-    let format = utilities.formatData(arr);
-    //console.log(format);
-
-    let parsed = utilities.parseData(format);
-    //console.log(parsed);
-
-    // then display the data as needed
-    const final = parsed.forEach(item => {
+// display the data in format as needed
+function displayData(data){
+    data.forEach(item => {
         const date = new Date(item.time)
         console.log(item.temp, date);
     });
-});
+};
+
+getData(displayData);
