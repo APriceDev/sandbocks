995deca6a0abf06710e62b6fa8216aae6642b975
APriceDev <alanprice.dev1@gmail.com>
2017-07-13 14:01:26 -0400

    added basic err checking to app.js

--- poc/dataparser/app.js
+++ poc/dataparser/app.js
@@ -1,26 +1,23 @@
+
 const fs = require('fs');
 const utilities = require('./modules/utilities');
-
-//utilities.foo();
 
 let file = 'data/' + process.argv[2];
 
 fs.readFile(file, 'utf8', function(err, data){
 
-    // removes trailing comma JSON.parse() does not allow trailing commas
+    if(err) return  console.log(err);
+
    let trim = utilities.removeComma(data);
    //console.log(trim);
 
-    // trimmed items into array
     let arr = utilities.itemsToArray(trim);
     //console.log(arr);
 
-    // format items,  ready for JSON
-    let format = utilities.format(arr);
+    let format = utilities.formatData(arr);
     //console.log(format);
 
-    // JSON parse
-    let parsed = utilities.parsed(format);
+    let parsed = utilities.parseData(format);
     //console.log(parsed);
 
     // then display the data as needed
