e78da47df385a246c383875039679c9467b5e695
APriceDev <alanprice.dev1@gmail.com>
2017-07-14 14:50:11 -0400

    added displayData method to utilities module

--- poc/dataparser/modules/utilities.js
+++ poc/dataparser/modules/utilities.js
@@ -1,17 +1,27 @@
 module.exports = {
+    // removes trailing comma JSON.parse() does not allow trailing commas
     removeComma : (data) => {
         return data.replace(/[\s,]+$/g, '');
     },
+    // trimmed items into array
     itemsToArray : (data) => {
         return data.split(",").map(item => item.trim());
     },
-    format : (data) => {
+    // format items,  ready for JSON
+    formatData : (data) => {
         return data.map(item => `{"temp" : ${item.split(" ")[0]}, "time" : ${item.split(" ")[1]}}`);
     },
-    parsed : (data) => {
+    // JSON parse
+    parseData : (data) => {
         const dataArray = [];
         data.forEach(item => dataArray.push(JSON.parse(item)));
         return dataArray;
     },
+    // display the data in format as needed
+    displayData : (data) => {
+        data.forEach(item => {
+            const date = new Date(item.time)
+            console.log(item.temp, date);
+        });
+    }
 };
-
