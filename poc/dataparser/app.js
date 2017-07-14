c1cc4018d0324419178022822be59b7bd845bfe8
APriceDev <alanprice.dev1@gmail.com>
2017-07-14 14:52:57 -0400

    refactor app.js to reflect updated utilities module

--- poc/dataparser/app.js
+++ poc/dataparser/app.js
@@ -4,23 +4,20 @@ const utilities = require('./modules/utilities');
 let pathToFile = process.argv[2];
 
 function getData(callback){
-    let trim, arr, format, parsed;
     fs.readFile(pathToFile, 'utf8', function(err, data){
         if(err) throw err;
-        trim = utilities.removeComma(data);
-        arr = utilities.itemsToArray(trim);
-        format = utilities.formatData(arr);
-        parsed = utilities.parseData(format);
-        callback(parsed);
+        callback(data);
     });
 };
 
-// display the data in format as needed
-function displayData(data){
-    data.forEach(item => {
-        const date = new Date(item.time)
-        console.log(item.temp, date);
-    });
+function parseData(data){
+    let trim, arr, format, parsed;
+
+    trim = utilities.removeComma(data);
+    arr = utilities.itemsToArray(trim);
+    format = utilities.formatData(arr);
+    parsed = utilities.parseData(format);
+    utilities.displayData(parsed);
 };
 
-getData(displayData);
+getData(parseData);
